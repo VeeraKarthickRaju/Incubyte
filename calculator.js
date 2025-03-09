fac3d2ca706a83c5b6afff6ed45e94e4ca1e9775
VeeraKarthickRaju <48280311+VeeraKarthickRaju@users.noreply.github.com>
2025-03-09 21:09:48 +0530

    Updated to handle new line character.

--- calculator.js
+++ calculator.js
@@ -4,11 +4,11 @@ function add(numbers) {
     return 0;
   }
 
+  // Split the string by either commas or newlines
   return numbers
-    .split(',')
-    .map(Number)  // Convert each string to a number
+    .split(/[\n,]/)  // Regular expression to split by comma or newline
+    .map(Number)     // Convert each item in the array to a number
     .reduce((sum, num) => sum + num, 0);  // Sum up all the numbers
 }
 
-// Export the add function to make it available for testing
-module.exports = { add };
+module.exports = { add };  // Export the add function for use in tests
