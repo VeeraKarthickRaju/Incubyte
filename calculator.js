054bb5c92ad46f7dda8f43aaa01b7a59399e2f6a
VeeraKarthickRaju <48280311+VeeraKarthickRaju@users.noreply.github.com>
2025-03-09 21:19:37 +0530

    Final commit to handle any delimiter and Negative numbers

--- calculator.js
+++ calculator.js
@@ -1,14 +1,31 @@
 // Add method to handle any amount of numbers
 function add(numbers) {
+  // If the string is empty, return 0
   if (numbers === "") {
     return 0;
   }
 
-  // Split the string by either commas or newlines
-  return numbers
-    .split(/[\n,]/)  // Regular expression to split by comma or newline
-    .map(Number)     // Convert each item in the array to a number
-    .reduce((sum, num) => sum + num, 0);  // Sum up all the numbers
+  // Regex to match all numbers in the string (including negative numbers)
+  const numArray = numbers.match(/-?\d+/g);
+
+  // If no numbers are found, return 0
+  if (!numArray) {
+    return 0;
+  }
+
+  // Filter out negative numbers and store them
+  const negativeNumbers = numArray.filter(num => num < 0);
+
+  // If there are negative numbers, throw an exception with them
+  if (negativeNumbers.length > 0) {
+    throw new Error(`Negatives not allowed: ${negativeNumbers.join(', ')}`);
+  }
+
+  // Convert the array of numbers (strings) to an array of integers and sum them up
+  return numArray
+    .map(Number)  // Convert all elements to numbers
+    .reduce((sum, num) => sum + num, 0);  // Sum them up
 }
 
 module.exports = { add };  // Export the add function for use in tests
+
